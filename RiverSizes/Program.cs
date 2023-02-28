9c8675c543a30122e6c77c939449d52ee29e3bf5
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-28 01:33:33 +0100

    river size, initialization

---
+++ RiverSizes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RiverSizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]{ {1, 0, 0, 1, 0},
+                                        {1, 0, 1, 0, 0},
+                                        {0, 0, 1, 0, 1},
+                                        {1, 0, 1, 0, 1},
+                                        {1, 0, 1, 1, 0}};
+
+            Console.WriteLine("River sizes are : ");
+            var riverSizes = RiverSizes(matrix);
+
+            foreach (var size in riverSizes)
+            {
+                Console.WriteLine(size);
+            }
+        }
+
+        public static List<int> RiverSizes(int[,] matrix)
+        {
+
+            return new List<int>() { 1 , 5};
+        }
+    }
+}
