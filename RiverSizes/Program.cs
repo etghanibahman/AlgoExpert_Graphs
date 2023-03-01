c3f9654e4c342de89694ce71b8aa0557f41acac9
Seyed Ali Etghani <51031083+etghanibahman@users.noreply.github.com>
2023-03-01 16:46:11 +0100

    Update Program.cs

--- RiverSizes/Program.cs
+++ RiverSizes/Program.cs
@@ -22,10 +22,73 @@ namespace RiverSizes
             }
         }
 
+        /// <summary>
+        /// Time = O(wh) , Space = O(wh)
+        /// </summary>
         public static List<int> RiverSizes(int[,] matrix)
         {
+            int height = matrix.GetLength(0);
+            int width = matrix.GetLength(1);
+            bool[,] visitedMatrix = new bool[height, width];
+            Stack<int[]> coordOfSameRiver = new Stack<int[]>();
+            List<int> ret = new List<int>();
 
-            return new List<int>() { 1 , 5};
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    if (visitedMatrix[row,col])
+                        continue;
+                    if (matrix[row,col] == 1)
+                    {
+                        visitedMatrix[row, col] = true;
+                        coordOfSameRiver.Push(new int[2] { row,col});
+                        ret.Add(ComputeSizeOfCurrentRiver(matrix,coordOfSameRiver,visitedMatrix));
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        public static int ComputeSizeOfCurrentRiver(int[,] matrix, Stack<int[]> coordOfSameRiver, bool[,] visitedMatrix) { 
+            int size = 0;
+            int height = matrix.GetLength(0);
+            int width = matrix.GetLength(1);
+
+            while (coordOfSameRiver.Count > 0)
+            {
+                var current = coordOfSameRiver.Pop();
+                int row = current[0];
+                int col = current[1];
+                size++;
+
+                if (row -1 >= 0 && !visitedMatrix[row - 1,col] && matrix[row - 1, col] == 1)
+                {
+                    visitedMatrix[row - 1, col] = true;
+                    coordOfSameRiver.Push(new int[2] { row - 1, col});
+                }
+
+                if (col + 1 < width && !visitedMatrix[row, col + 1] && matrix[row, col + 1] == 1)
+                {
+                    visitedMatrix[row, col + 1] = true;
+                    coordOfSameRiver.Push(new int[2] { row , col + 1 });
+                }
+
+                if (row + 1 < height && !visitedMatrix[row + 1, col] && matrix[row + 1, col] == 1)
+                {
+                    visitedMatrix[row + 1, col] = true;
+                    coordOfSameRiver.Push(new int[2] { row + 1, col });
+                }
+
+                if (col - 1 > 0 && !visitedMatrix[row, col - 1] && matrix[row, col - 1] == 1)
+                {
+                    visitedMatrix[row, col - 1] = true;
+                    coordOfSameRiver.Push(new int[2] { row, col - 1 });
+                }
+            }
+
+            return size;
         }
     }
 }
