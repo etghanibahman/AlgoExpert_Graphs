6bfb30d58299a7abd7875acb68fd8ee752db9735
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-03 18:14:42 +0100

    cycle in graph, initialization

---
+++ CycleInGraph/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CycleInGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] edges = { new int[] { 1, 3 }, new int[] { 2, 3, 4 }, new int[] { 0 }, 
+                                new int[] { }, new int[] { 2, 5 }, new int[] { } };
+
+            
+
+            Console.WriteLine($"Is there a cycle in the graph? {CycleInGraph(edges)}");
+        }
+
+        public static bool CycleInGraph(int[][] edges)
+        {
+            // Write your code here.
+            return false;
+        }
+    }
+}
