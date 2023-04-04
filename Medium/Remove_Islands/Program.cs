79db1a2aa845ff01d5ccec1d6ebfbafdc0e44155
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-04-04 23:32:46 +0200

    remove islands

--- Medium/Remove_Islands/Program.cs
+++ Medium/Remove_Islands/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Linq;
 
 namespace Remove_Islands
@@ -23,19 +24,90 @@ namespace Remove_Islands
 
             var result = RemoveIslands(matrix);
 
-            //Console.WriteLine("\nThe output matrix is:");
-            //for (int i = 0; i < matrix.GetLength(0); i++)
-            //{
-            //    Console.WriteLine(string.Join(",", matrix[i]));
-            //}
-
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("\nThe output matrix is:");
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                Console.WriteLine(string.Join(",", result[i]));
+            }
         }
 
         public static int[][] RemoveIslands(int[][] matrix)
         {
-            // Write your code here.
-            return new int[][] { };
+            Console.WriteLine($"rows : {matrix.Length} | cols: {matrix[0].Length}");
+            var notIslands = new bool[matrix.Length,matrix[0].Length];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    bool rowIsBorder = row == 0 || row == matrix.Length - 1;
+                    bool colIsBorder = col == 0 || col == matrix[row].Length - 1;
+                    bool isBorder = rowIsBorder || colIsBorder;
+                    if (!isBorder)
+                    {
+                        continue;
+                    }
+                    if (matrix[row][col] != 1)
+                    {
+                        continue;
+                    }
+                    DFS(matrix,row,col,notIslands);
+                }
+            }
+
+            Console.WriteLine("\notIslands is:");
+            for (int i = 0; i < notIslands.GetLength(0); i++)
+            {
+                Console.WriteLine(string.Join(",", GetRow(notIslands, i)));
+            }
+
+            for (int row = 0; row < matrix.Length - 1; row++)
+            {
+                for (int col = 0; col < matrix[row].Length - 1; col++)
+                {
+                    if (matrix[row][col] == 1 && notIslands[row,col] == false)
+                    {
+                        matrix[row][col] = 0;
+                    }
+                }
+            }
+
+            return matrix;
         }
+
+        public static void DFS(int[][] matrix, int row, int col, bool[,] notIslands)
+        {
+            if (matrix[row][col] == 0 || notIslands[row,col] == true)
+            {
+                return;
+            }
+            notIslands[row, col] = true;
+
+            //Search up
+            if (row > 0)
+            {
+                DFS(matrix, row -1, col, notIslands);
+            }
+            //Search down
+            if (row < matrix.Length -1)
+            {
+                DFS(matrix, row + 1, col, notIslands);
+            }
+            //Search left
+            if (col > 0)
+            {
+                DFS(matrix, row, col-1, notIslands);
+            }
+            //Search right
+            if (col < matrix[0].Length -1)
+            {
+                DFS(matrix, row , col + 1, notIslands);
+            }
+        }
+
+        public static bool[] GetRow(bool[,] mat, int rowNumber)
+  => Enumerable.Range(0, mat.GetLength(1))
+          .Select(i => mat[rowNumber, i])
+          .ToArray();
     }
 }
